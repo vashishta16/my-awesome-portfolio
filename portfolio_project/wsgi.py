5318b8ad805d94d07e13e2568a301304298d3698
vashishta16 <vashishtareddym16@gmail.com>
2026-08-22 17:32:39 +0530

    Fix Vercel deployment

--- portfolio_project/wsgi.py
+++ portfolio_project/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
 
 application = get_wsgi_application()
+
+# Vercel requires 'app' to be the WSGI application
+app = application
